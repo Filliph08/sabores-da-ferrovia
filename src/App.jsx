34ffc63d22ddcb4f8626b219b90370087b75207a
mielliph <mielliph@gmail.com>
2025-06-12 19:55:34 -0300

    feat: adcionei as imagens e centralizei elas

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,24 @@
 function App() {
   return (
     <header>
+      <div>
       <div className="Cabeça">
-        <img src="./img/logo.png" alt="Sabores da ferrovia" /> 
+        <img src="./img/logo.png" alt="Sabores da ferrovia" width={400} height={400} className="object-contain"/>
+        <div class="flex items-center">
+          </div> 
       </div>
+      <div className="Home"> 
+        <img src="./img/facebook.png" alt="Login Facebook" width={40} height={40} className="object-contain"/>
+        <div className="flex items-end"></div>
+  </div>
+        </div>
+        
+
+
+
+
+
+
     </header>
   );
 }
