56556b54417a33de68610968e5bf27f5bd9c7206
mielliph <mielliph@gmail.com>
2025-06-13 19:59:11 -0300

    feat: adicionei o insta e pizza na home

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,14 +1,16 @@
 export const Footer = () => {
     return (
-        <footer className="flex items-center gap-4 justify-start w-full p-8">
-            <img src="./img/facebook.png" alt="Login Facebook" width={40} height={40} className="object-contain" />
-            
-            
-            < footer className="flex items-center gap-4 justify-center w-full p-4">
-                <img src="./img/insta.png" alt="login insta" width={40} height={40} className="object-contain" />
+        <footer className="flex items-center gap-1 justify-start w-full p-8">
+            <img src="./img/facebook.png" alt="Login Facebook" width={35} height={35} className="object-contain" />
 
+
+            < footer className="flex items-center gap-0 justify-start w-full p-4">
+                <img src="./img/insta.png" alt="login insta" width={35} height={35} className="object-contain" />
             </footer>
 
+            < footer className="flex items-center gap-0 justify-start w-full p-4">
+                <img src="./img/pizza.png" alt="pizza" width={900} height={50} className="object-contain" />
+            </footer>
 
 
         </footer>
