3f6c3fa376afbabdc094c991d98cd4228dbd34a3
casarim <kisutwhatever@gmail.com>
2025-06-13 20:02:39 -0300

    feat:adiconei a logo nova

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,10 +4,12 @@ export const HomePage = () => {
     return (
         <div className="flex flex-col justify-between h-screen">
             <div>
-                <img src="./img/logo.png" alt="Sabores da ferrovia" width={200} height={200} className="object-contain" />
+                <img src="./img/logonova.png" alt="Sabores da ferrovia" width={200} height={200} className="object-contain" />
                 <div class="flex items-center">
+            
                 </div>
             </div>
+
             <Footer />
         </div>
     );
