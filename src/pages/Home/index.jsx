7f1ff90679ea4703d4bc7cebaafe131292c5a2ad
mielliph <mielliph@gmail.com>
2025-06-16 19:40:26 -0300

    feat: o prefessor refez a parte de cima do site usando outra linguagem

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -8,19 +8,24 @@ export const HomePage = () => {
                 <div className="header-logo">
                     <img src="./img/logonova.png" alt="Sabores da ferrovia" width={200} height={200} className="object-contain" />
                 </div>
-                <div className="header-pesquisa">
-                    <nav className="search-bar">
-                        <i class="fa-solid fa-magnifying-glass"></i>
-                        <input type="text" placeholder="Busque seu restaurante" className="search-input" />
-                    </nav>
 
+                <div className="w-full p-4">
+                    <form>
+                        <fieldset className="w-full max-w-lg">
+                            
+                            <input type="text" name='search' id='search' className="w-full p-3 rounded-lg border border-primary focus:outline-none focus:border-white transition-all ease-in-out" placeholder="Busque seu restaurante"/>
+                        </fieldset>
+                    </form>
+                </div>
+
+                <div>
                     <nav className="list-nav">
                         <ul>
                             <li>
                                 <a href="#"><img src="./img/casinha.png" alt="" /></a>
                             </li>
                             <li>
-                                <img src="./img/coração.png" alt="" />
+                                <img src="./img/coração.png" alt="favoritos" />
                             </li>
                         </ul>
                     </nav>
