b3d443df0467714b90997624559e9d7a56bf7765
mielliph <mielliph@gmail.com>
2025-06-17 18:35:06 -0300

    feat:
    nada

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -12,21 +12,17 @@ export const HomePage = () => {
                 <div className="w-full p-4">
                     <form>
                         <fieldset className="w-full max-w-lg">
-                            <input type="text" name='search' id='search' className="w-full p-3 rounded-lg border border-primary focus:outline-none focus:border-white transition-all ease-in-out" placeholder="Busque seu restaurante"/>
+                            <input type="text" name='search' id='search' className="w-full p-3 rounded-lg border border-primary focus:outline-none focus:border-white transition-all ease-in-out" placeholder="Busque seu restaurante" />
                         </fieldset>
                     </form>
                 </div>
 
                 <div>
-                    <nav className="list-nav">
-                        <ul>
-                            <li>
-                                <a href="#"><img src="./img/casinha.png"/></a>
-                            </li>
-                            <li>
-                                <img src="./img/coração.png" alt="favoritos" />
-                            </li>
-                        </ul>
+                    <nav className=" w-full flex gap-4">
+                        <button>
+
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
+                        </button>
                     </nav>
                 </div>
             </div>
