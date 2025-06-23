bc0ffd2ea425ddf049643fae7d862711655b0134
mielliph <mielliph@gmail.com>
2025-06-23 18:48:56 -0300

    feat: tentativa de subir outra pagina

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { HomePage } from "./pages/Home";
 import { LoginPage } from "./pages/Login";
 import { CadastroPage } from "./pages/Cadastro";
+import { FormularioPage } from "./pages/formulario";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path='/' element={<HomePage />} />
         <Route path='/login' element={<LoginPage />} />
         <Route path='/cadastro' element={<CadastroPage />} />
+        <Route path='/formulario' element={<FormularioPage />} />
       </Routes>
     </BrowserRouter>
   );
