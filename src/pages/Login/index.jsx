6251d3633e8059e46cb2d03e3e3b58a4f7700f90
mielliph <mielliph@gmail.com>
2025-06-23 18:49:39 -0300

    feat: refiz a merda

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,9 +1,21 @@
 
 export const LoginPage = () => {
     return (
-        <main>
+        <main className="w-full flex items-center justify-center min-h-screen" style={{backgroundImage: "linear-gradient(to right, #721424, #833b47)"}}>
+            <div className="w-full max-w-xs bg-neutral-50 p-20 rounded-xl shadow-lg flex flex-col items-center">
+                <div class="flex flex-row space-y-4 font-bold"></div>
+                <h1 className="text-4xl text-primary justify-center flex mb-4">Login:</h1>
 
-            
+                <form>
+                    <p>Digite seu email:</p>
+                    <input className="border px-3 py-1 rounded" style={{ borderColor: "#721424" }} type="text" placeholder="Email..." />
+
+                    <p>Utilize sua senha:</p>
+                    <input className="border px-3 py-1 rounded" style={{ borderColor: "#721424" }} type="password" placeholder="Senha..." />
+
+                    <button className="text-white px-6 py-3 text-lg rounded shadow hover:opacity-90 transition" style={{ backgroundColor: "#721424" }}>Continue</button>
+                </form>
+            </div>
         </main>
     );
 }
